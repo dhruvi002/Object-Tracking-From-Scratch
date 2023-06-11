42223fa6d7a60eb41aaeb187686811deca0e87e9
dhruvi002 <80109665+dhruvi002@users.noreply.github.com>
2023-06-11 13:18:03 +0530

    Corrected added object id to each frame

--- obDet5.py
+++ obDet5.py
@@ -16,39 +16,37 @@ trackId = 0
 
 while True: 
     ret, frame = cap.read()
-
     count +=1
-
     if not ret: 
         break
-        
+
     #Point Current Frame    
     centerPointCurrentFrame = []
-
-
+ 
     #Detect objects on frame 
     (class_ids, scores, boxes) = od.detect(frame)
     for box in boxes: 
         (x,y,w,h) = box
 
-        cx = int((x+x+w)/2)
-        cy = int((y+y+h)/2)
+        cx = int(x + 0.5*w)
+        cy = int(y + 0.5*h)
         centerPointCurrentFrame.append((cx,cy))
 
         # print("FRAME NO", count, ': ', x, y, w, h)
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
         # cv2.circle(frame, (cx,cy), 5, (0,0,255), -1)
 
-    for pt in centerPointCurrentFrame:
-        cv2.circle(frame, pt, 5, (0,0,255), -1)
+    # for pt in centerPointCurrentFrame:
+    #     cv2.circle(frame, pt, 5, (0,0,255), -1)
     
-    for pt1 in centerPointCurrentFrame:
+    for pt in centerPointCurrentFrame:
         for pt2 in centerPointPrevFrame:
-            distance = math.hypot(pt2[0] - pt1[0], pt2[1] - pt1[1])
+            distance = math.hypot(pt2[0] - pt[0], pt2[1] - pt[1])
 
-            if distance < 50:
+            if distance < 10:
                 trackingObj[trackId] = pt
                 trackId+=1 
+                break
 
     for objectId, ptTrackObj in trackingObj.items():
         cv2.circle(frame, (cx,cy), 5, (0,0,255), -1)
